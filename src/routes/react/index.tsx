5d6eac59f24a1e73de5026b68d891e01b68663e2
dengninghai <2640559435@qq.com>
2021-01-20 21:14:13 +0800

    add model content

--- src/routes/react/index.tsx
+++ src/routes/react/index.tsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import MTree from '../../components/Tree';
 import { reactTreeData } from '../../config/treeData';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import MTable from '../../components/MTable';
 import Header from '../../components/Header';
 import MModel from '../../components/MModel';
 import { EllipsisOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
 
 import './index.less';
-import { Button } from 'antd';
 
 const UseReact: React.FC = () => {
   const [visible, setVisible] = useState<boolean>(false);
@@ -38,37 +36,45 @@ const UseReact: React.FC = () => {
       )
     }
   )
+
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className='reactWrapper'>
-        <MTree data={reactTreeData} />
-        <div className='right'>
-          <Header title='REACT'>
-            <div>
-              search
+    <div className='reactWrapper' >
+      <MTree data={reactTreeData} />
+      <div className='right'>
+        <Header title='REACT'>
+          <div>
+            search
             </div>
-          </Header>
-          <div className='tableContent'>
-            <MTable renderAction={renderAction} />
+        </Header>
+        <div className='tableContent'>
+          <MTable renderAction={renderAction} />
+        </div>
+      </div>
+      <MModel
+        width={1000}
+        closeModel={closeModel}
+        visible={visible}
+        title={'REACTRECORD'}
+        maskClosable={false}
+      >
+        <div className='model_Wrapper'>
+          <div className='content'>
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => {
+              return (
+                <div className='model-content'>
+                  <span className='contentTitle'>name:</span>
+                  <div className='modelInput'>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</div>
+                </div>
+              )
+            })}
+          </div>
+          <div className='actionWrapper'>
+            <Button className='modelBtn' type='primary' onClick={onSubmit}>确定</Button>
+            <Button className='modelBtn' onClick={() => closeModel && closeModel()}>取消</Button>
           </div>
         </div>
-        <MModel
-          width={1000}
-          closeModel={closeModel}
-          visible={visible}
-          title={'REACTRECORD'}
-          maskClosable={false}
-        >
-          <div className='model_Wrapper'>
-            <div className='content'>content</div>
-            <div className='actionWrapper'>
-              <Button className='submit' type='primary' onClick={onSubmit}>确定</Button>
-              <Button onClick={() => closeModel && closeModel()}>取消</Button>
-            </div>
-          </div>
-        </MModel>
-      </div>
-    </DndProvider>
+      </MModel>
+    </div >
   )
 };
 
