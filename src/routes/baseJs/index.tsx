6ba09c078843271f325b2940e211b4ef6119b249
dengninghai <2640559435@qq.com>
2021-01-21 20:15:27 +0800

    save tree selected position

--- src/routes/baseJs/index.tsx
+++ src/routes/baseJs/index.tsx
@@ -2,14 +2,28 @@ import { Input } from 'antd';
 import React from 'react';
 import Header from '../../components/Header';
 import MTree from '../../components/Tree';
+import { connect } from 'react-redux';
+import { setSelectTree } from '../../model/action';
 import { baseJsTreeData } from '../../config/treeData';
 
 import './index.less';
 
-const BaseJs: React.FC = () => {
+interface IProps {
+  selected: any;
+  setSelectTree: any;
+}
+
+const BaseJs: React.FC<IProps> = ({
+  selected, setSelectTree,
+}) => {
+
+  const selectItem = (data: string[]) => {
+    setSelectTree({ data, library: 'baseJs' });
+  }
+
   return (
     <div className='baseWrapper'>
-      <MTree data={baseJsTreeData} />
+      <MTree data={baseJsTreeData} library='baseJs' selectItem={selectItem} selected={selected.baseJs} />
       <div className='right'>
         <Header title={'BASEJS'}>
           <Input className='baseInput' />
@@ -20,4 +34,9 @@ const BaseJs: React.FC = () => {
   )
 }
 
-export default BaseJs;
+export default connect(
+  (state: any) => ({
+    selected: state.treeReducer,
+  }),
+  { setSelectTree }
+)(BaseJs);
