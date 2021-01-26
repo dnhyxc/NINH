1e5e06bdba85f15c9bb26355788aa25a93c6cebf
dengninghai <2640559435@qq.com>
2021-01-26 23:56:19 +0800

    add login

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -1,16 +1,47 @@
 import React from 'react';
-import { Carousel } from 'antd';
+import { Tabs, Input, Button } from 'antd';
+import icon from '../../assets/image/logo192.png';
 import './index.less';
 
+const { TabPane } = Tabs;
+
 const Login: React.FC = () => {
+  const onTabChange = () => {
+    console.log(11111);
+  }
+
   return (
     <div className='login'>
-      <Carousel autoplay effect="fade" className='carousel' dots={false}>
-        <img className='carousel-image' src="https://pic4.zhimg.com/80/v2-7fbdb98c77a0efd4de69c05dfaa7fdd9_720w.jpg?source=1940ef5c" alt="" />
-        <img className='carousel-image' src="https://pic2.zhimg.com/80/v2-23a62728f9364b6868d10d9607392c7e_720w.jpg?source=1940ef5c" alt="" />
-        <img className='carousel-image' src="https://pic1.zhimg.com/80/v2-82a2709b2e63487e2fb605995387f579_720w.jpg?source=1940ef5c" alt="" />
-      </Carousel>
-    </div>
+      <div className='login-info'>
+        <div className='logo'>
+          <div className='logoImg'>
+            <img className='icon' src={icon} alt="" />
+          </div>
+          <div className='logoText'>FRONTEND</div>
+        </div>
+        <div className='userInfo'>
+          <Tabs defaultActiveKey="1" onChange={onTabChange} className='loginTabs' animated>
+            <TabPane tab={<div className='account'>账号登录</div>} key="1">
+              <div className='act'>
+                <Input placeholder='请输入账号' className='actInp' />
+              </div>
+              <div className='act'>
+                <Input placeholder='请输入密码' className='actInp' />
+              </div>
+            </TabPane>
+            <TabPane tab={<div className='account'>手机快捷登录</div>} key="2">
+              <div className='act'>
+                <Input placeholder='请输入手机号' className='actInp' />
+              </div>
+              <div className='act'>
+                <Input placeholder='请输入验证码' className='actInp' />
+              </div>
+            </TabPane>
+          </Tabs>
+        </div>
+        <Button className='loginSubmit'>登录</Button>
+      </div>
+    </div >
   )
 }
 
